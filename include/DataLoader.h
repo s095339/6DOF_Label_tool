6a665502ddd2d0d54bcf619ecd6bb790578a0fb8
m111061549 <m111061549@m111.nthu.edu.tw>
2024-05-26 18:08:26 +0800

    [add]create dataloader

---
+++ include/DataLoader.h
@@ -0,0 +1,38 @@
+#ifndef _DATALOADER_H_  
+
+#define _DATALOADER_H_
+
+//class declaration
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <string>
+
+#include <vector>
+#include <map>
+
+class DataLoader{
+private:
+    std::string datapath;
+    std::vector<std::string> Image_path_list;
+    cv::Mat Camera_intrinsic;
+    cv::Mat dist;
+    void natsort();
+
+public:
+    DataLoader(std::string);
+    
+    std::string get_item(int idx);
+    std::string operator[](int idx);
+
+    //setter & getter
+    void set_Camera_intrinsic(cv::Mat);
+    cv::Mat get_Camera_intrinsic();
+    void set_Camera_dist(cv::Mat);
+    cv::Mat get_Camera_dist();
+    //不是那麼重要的
+    size_t length();
+};
+
+
+#endif
---
+++ src/DataLoader.cpp
@@ -0,0 +1,72 @@
+#include "DataLoader.h"
+#include "natural_sort.hpp"
+//constructor initialization list
+
+using namespace std;
+namespace fs = std::filesystem;
+
+DataLoader::DataLoader(std::string datapath)
+:datapath{datapath}
+{
+    //vector<string> img_path_list = {};
+
+    cout << "read image ... " << endl;
+    for (const auto & entry : fs::directory_iterator(this->datapath)){
+        //std::cout << entry.path() << std::endl;
+        string entrypath = string(entry.path());
+        std::size_t found = entrypath.rfind(".");
+        if(string(entry.path()).substr(found) != ".png") continue;
+        this->Image_path_list.push_back(entry.path());
+    
+    }
+    //for debug======================
+    /*
+    cv::Mat image;
+    for(const auto & img_path : img_path_list){
+        image = cv::imread( img_path, cv::IMREAD_COLOR );
+        cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+        cv::imshow("Display Image", image);
+        cv::waitKey(0);
+    }
+    */
+
+    //this->Image_path_list 
+    this->natsort();
+}
+
+void DataLoader::set_Camera_intrinsic(cv::Mat intr){
+    this->Camera_intrinsic = intr;
+}
+
+cv::Mat DataLoader::get_Camera_intrinsic(){
+    return this->Camera_intrinsic;
+}
+
+void DataLoader::set_Camera_dist(cv::Mat dist){
+    this->dist = dist;
+}
+
+cv::Mat DataLoader::get_Camera_dist(){
+    return this->dist;
+}
+
+
+
+void DataLoader::natsort(){
+
+    SI::natural::sort(this->Image_path_list);
+
+}
+
+std::string DataLoader::get_item(int idx){
+    return this->Image_path_list.at(idx);
+}
+
+std::string  DataLoader::operator[](int idx){
+    return this->Image_path_list.at(idx);
+}
+
+
+size_t DataLoader::length(){
+    return this->Image_path_list.size();
+}
